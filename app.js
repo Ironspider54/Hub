2bbaf0f68978655071e60f945d341d910b3e688e
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-02-28 18:50:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 function redirect(url) {
+  window.location.href = url;
+}
+
+function toggleMode() {
+  const body = document.querySelector('body');
+  const toggle = document.querySelector('.toggle input[type="checkbox"]');
+  
+  if (toggle.checked) {
+    body.classList.add('dark');
+  } else {
+    body.classList.remove('dark');
+  }
+}
