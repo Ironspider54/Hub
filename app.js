40af3ee142645b1b4ae22103e1655e5cd61750ab
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-03-01 16:14:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
-const toggleButton = document.getElementById('toggle-button');
-const container = document.getElementById('container');
+/* eslint-env browser */
+const toggleButton = document.querySelector('.toggle-button');
+const body = document.querySelector('body');
+const confirmationDialog = document.getElementById('confirmation-dialog');
+const confirmationDialogYes = document.getElementById('confirmation-dialog-yes');
+const confirmationDialogNo = document.getElementById('confirmation-dialog-no');
 
-toggleButton.addEventListener('click', function() {
-  container.classList.toggle('dark');
-  
-  if (container.classList.contains('dark')) {
-    if (confirm("Are you sure you want to switch to dark mode?")) {
-      // user clicked OK
-    } else {
-      container.classList.remove('dark');
-    }
+function showConfirmationDialog() {
+  confirmationDialog.style.display = 'flex';
+}
+
+function hideConfirmationDialog() {
+  confirmationDialog.style.display = 'none';
+}
+
+function toggleTheme() {
+  showConfirmationDialog();
+}
+
+function setLightTheme() {
+  body.classList.remove('dark');
+}
+
+function setDarkTheme() {
+  body.classList.add('dark');
+}
+
+confirmationDialogYes.addEventListener('click', () => {
+  hideConfirmationDialog();
+  const isDarkMode = body.classList.contains('dark');
+  if (isDarkMode) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
   }
 });
+
+confirmationDialogNo.addEventListener('click', () => {
+  hideConfirmationDialog();
+});
+
+toggleButton.addEventListener('click', toggleTheme);
