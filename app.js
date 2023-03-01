0d8373bf2254c212fa486597165d669e1936f4ee
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-03-01 11:37:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-function toggleMode() {
-  const body = document.querySelector('body');
-  const toggle = document.querySelector('.toggle input[type="checkbox"]');
-  
-  if (toggle.checked) {
-    body.classList.add('dark');
+const toggleSwitch = document.querySelector('#mode-switch');
+
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
   } else {
-    body.classList.remove('dark');
+    document.documentElement.setAttribute('data-theme', 'light');
   }
 }
+
+function confirmRedirect(event) {
+  if (!confirm("Are you sure you want to go to this website?")) {
+    event.preventDefault();
+  }
+}
+
+const links = document.querySelectorAll('a');
+links.forEach(link => link.addEventListener('click', confirmRedirect));
+
+toggleSwitch.addEventListener('change', switchTheme, false);
