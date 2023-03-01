93b255a1c7eb874c643bbd71110de13bb5e249f1
Ironspider54 <103035325+Ironspider54@users.noreply.github.com>
2023-03-01 11:47:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const toggleSwitch = document.querySelector('#mode-switch');
+const toggleButton = document.getElementById('toggle-button');
+const container = document.getElementById('container');
 
-function switchTheme(event) {
-  if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+toggleButton.addEventListener('click', function() {
+  container.classList.toggle('dark');
+  
+  if (container.classList.contains('dark')) {
+    if (confirm("Are you sure you want to switch to dark mode?")) {
+      // user clicked OK
+    } else {
+      container.classList.remove('dark');
+    }
   }
-}
-
-function confirmRedirect(event) {
-  if (!confirm("Are you sure you want to go to this website?")) {
-    event.preventDefault();
-  }
-}
-
-const links = document.querySelectorAll('a');
-links.forEach(link => link.addEventListener('click', confirmRedirect));
-
-toggleSwitch.addEventListener('change', switchTheme, false);
+});
